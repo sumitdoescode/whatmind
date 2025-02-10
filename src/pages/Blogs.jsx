1850318e63b1c8962ab33b6fdfcf4b632779046d
Sumit Raj <sumit.does.code@gmail.com>
2025-02-10 14:19:38 +0530

    Added blogs

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -6,29 +6,12 @@ const Blogs = () => {
         <Section className="pt-32">
             <SmallContainer>
                 <SectionHeading className="">.blogs</SectionHeading>
-                <div className="mt-10">
-                    <Content>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus quod autem ex illo porro, nesciunt maiores, et excepturi, vel quas officiis non. Obcaecati veritatis
-                        praesentium nobis esse sint? Nesciunt laborum dolor fugiat tempora molestiae hic unde dolorum odio facere distinctio harum soluta recusandae voluptatibus cum atque
-                        necessitatibus, molestias eveniet voluptatem quisquam, id numquam rem! Facilis optio non sunt quia similique tempore expedita recusandae excepturi perspiciatis rerum hic
-                        quaerat, sed quos numquam! Deleniti ad quisquam facilis non illo cumque inventore asperiores hic quidem quasi quam itaque fugit ducimus libero, suscipit unde aliquid eius sint.
-                        Dignissimos quia, cupiditate nulla sit eligendi magnam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quam fuga aperiam, quia vel distinctio itaque. Eveniet
-                        adipisci minus, eligendi commodi voluptatum maxime excepturi dicta optio reiciendis fugiat
-                        <br />
-                        <br />
-                        {/* <br /> */}
-                        minima ea modi, neque non cupiditate asperiores error doloremque veniam cumque, officiis atque at ab! Illo vitae, debitis aliquid iusto rerum recusandae hic sapiente eius
-                        quisquam beatae! Animi explicabo sed velit, ad a non laborum numquam optio vero eaque consequatur dicta officiis quas voluptas nam quasi
-                        {/* <br /> */}
-                        <br />
-                        <br />
-                        exercitationem aliquam, cupiditate consectetur, voluptatum eius iusto placeat accusantium molestias? Sit molestias unde ex laudantium sapiente id blanditiis atque dolorum
-                        quidem assumenda quas nobis illum repellendus delectus neque pariatur, minus impedit a odit autem quaerat! Quis voluptates harum quasi laborum perferendis porro. Exercitationem
-                        commodi quaerat aut voluptatibus officiis sit dolorem numquam quod maxime facilis deleniti, fugiat dolorum magnam consequuntur cumque repellendus accusamus porro aspernatur
-                        totam id, laudantium nam praesentium quibusdam? Exercitationem maxime, impedit perferendis nisi nesciunt eligendi optio rerum aliquam laudantium omnis blanditiis ipsum. Eius
-                        perspiciatis laboriosam nostrum laborum enim, veniam voluptas nam repellat at voluptates molestiae modi alias, adipisci aut ipsum animi ullam! Fugit pariatur ipsum quasi
-                        deserunt autem iure odio maiores dignissimos quos aliquid, reprehenderit cum ducimus adipisci ratione, ipsa, commodi laboriosam quis dolore?
-                    </Content>
+                <div className="mt-10 flex flex-col gap-8">
+                    <img src="/images/1.jpg" alt="" />
+                    <img src="/images/2.jpg" alt="" />
+                    <img src="/images/3.jpg" alt="" />
+                    <img src="/images/4.jpg" alt="" />
+                    <img src="/images/5.jpg" alt="" />
                 </div>
             </SmallContainer>
         </Section>
